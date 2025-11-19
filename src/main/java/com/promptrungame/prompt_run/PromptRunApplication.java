fffdf77a6cd1092e2505bcf4d7046f3d066366ad
ProteinJ <jaehyun516@naver.com>
2025-11-19 23:28:59 +0900

    "init: 프로젝트 초기 환경 및 구조 설정"

---
+++ src/main/java/com/promptrungame/prompt_run/PromptRunApplication.java
@@ -0,0 +1,13 @@
+package com.promptrungame.prompt_run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptRunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/promptrungame/prompt_run/PromptRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promptrungame.prompt_run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
